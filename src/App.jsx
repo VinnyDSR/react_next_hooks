994200a5870a15a41b5c40681c52bda411549d27
VinnyDSR <r.viniciusds@gmail.com>
2023-06-28 00:37:05 -0300

    03_hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-import HookState01 from './components/UseState01'
-import HookState02 from './components/UseState02'
+//import HookState01 from './components/UseState01'
+//import HookState02 from './components/UseState02'
+import HookState03 from './components/UseState03'
 
 export default () => {
+
+
     return (
         <>
             <h1>REACT HOOKS</h1>
             {/*<HookState01/>*/}
-            <HookState02/>
-            
+            {/*<HookState02/>*/}
+            <HookState03/>
         </>
     )
 
