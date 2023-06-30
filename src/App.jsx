8799e4529225bf469a41e5686b37912f3bf2d587
VinnyDSR <r.viniciusds@gmail.com>
2023-06-29 23:50:06 -0300

    05_branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 //import HookState01 from './components/UseState01'
 //import HookState02 from './components/UseState02'
 //import HookState03 from './components/UseState03'
-import HookState04 from './components/UseState04'
+//import HookState04 from './components/UseState04'
+import HookState05 from './components/UseState05'
 
 export default () => {
 
@@ -14,7 +15,8 @@ export default () => {
             {/*<HookState01/>*/}
             {/*<HookState02/>*/}
             {/*<HookState03/>*/}
-            <HookState04/>
+            {/*<HookState04/>*/}
+            <HookState05/>
         </>
     )
 
